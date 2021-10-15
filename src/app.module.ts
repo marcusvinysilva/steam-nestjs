332473688cc94a162ba7e6ad8c936ee5030461f3
Marcus <mvsilva1997@gmail.com>
2021-10-14 23:16:55 -0300

    config typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfig } from './configs/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(TypeOrmConfig)],
   controllers: [],
   providers: [],
 })
