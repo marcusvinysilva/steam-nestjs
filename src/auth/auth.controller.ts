e19adedfd72e0a4c0291ebdc43b684089319f8bc
Marcus <mvsilva1997@gmail.com>
2021-10-21 19:48:59 -0300

    update auth routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,77 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  ValidationPipe,
+  Get,
+  UseGuards,
+  Patch,
+  Param,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from 'src/users/dtos/create-user.dto';
+import { CredentialsDto } from './credentials.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from 'src/users/user.entity';
+import { GetUser } from './get-user.decorator';
+import { ChangePasswordDto } from './change-password.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('signup')
+  async signUp(
+    @Body(ValidationPipe) createUserDto: CreateUserDto,
+  ): Promise<{ message: string }> {
+    await this.authService.signUp(createUserDto);
+    return {
+      message: 'Cadastro realizado com sucesso',
+    };
+  }
+
+  @Post('signin')
+  async singIn(
+    @Body(ValidationPipe) credentialsDto: CredentialsDto,
+  ): Promise<{ token: string }> {
+    return await this.authService.signIn(credentialsDto);
+  }
+
+  @Patch(':token')
+  async confirmEmail(@Param('token') token: string) {
+    await this.authService.confirmEmail(token);
+    return { message: 'Email confirmado' };
+  }
+
+  @Post('send-recover-email')
+  async sendRecoverPasswordEmail(
+    @Body('email') email: string,
+  ): Promise<{ message: string }> {
+    await this.authService.sendRecoverPasswordEmail(email);
+    return {
+      message: 'Foi enviado um email com instruções para resetar a sua senha',
+    };
+  }
+
+  @Patch('reset-password/:token')
+  async resetPassword(
+    @Param('token') token: string,
+    @Body(ValidationPipe) changePasswordDto: ChangePasswordDto,
+  ): Promise<{ message: string }> {
+    await this.authService.resetPassword(token, changePasswordDto);
+    return {
+      message: 'Senha alterada com sucesso',
+    };
+  }
+
+  @Get('me')
+  @UseGuards(AuthGuard())
+  getMe(@GetUser() user: User): User {
+    return user;
+  }
+
+  // Fazer
+  // Update do usuário logado
+  // Delete da conta do usuário logado
+  // Buscar outros usuários apenas pelo nome
+}
